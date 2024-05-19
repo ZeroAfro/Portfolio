517211b43deeffb6bcbb01d0a8f575d0e982d816
Beau Guthery <bguthery@gmail.com>
2024-05-19 11:14:35 -0500

    Update Overtime_Calculator.py

--- Coding/Python/Overtime_Calculator/Overtime_Calculator.py
+++ Coding/Python/Overtime_Calculator/Overtime_Calculator.py
@@ -24,20 +24,33 @@ def beginning_prompt():
 
 while True:
 
-  hours_worked = float(input("How many hours have you worked this week?: "))
+  try:
+    
+    hours_worked = float(input("How many hours have you worked this week?: "))
+
+  except ValueError:
+    print("Please enter a number.")
+    continue
 
   #Checking if the user has worked more than 40 hours or less than
   if hours_worked > 40:
-
-    hourly_wage = float(input("What is your hourly wage?: "))
-
-    overtime_hours = float(hours_worked - 40)
-    overtime_pay = float(overtime_hours *1.5 * hourly_wage)
     
-    print(f"You have are owned ${overtime_pay:.2f} overtime pay.")
+    while True:
 
-    beginning_prompt()
+      try:
+        hourly_wage = float(input("What is your hourly wage?: "))
 
-  elif hours_worked < 40:
+        overtime_hours = float(hours_worked - 40)
+        overtime_pay = float(overtime_hours *1.5 * hourly_wage)
+
+        print(f"You have are owned ${overtime_pay:.2f} overtime pay.")
+        beginning_prompt()
+
+      except ValueError:
+        print("Please enter a number.")
+        continue
+
+
+  elif hours_worked <= 40:
     print("You are not due any overtime pay.")
     beginning_prompt()
