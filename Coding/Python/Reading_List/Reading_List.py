e77dcc600a52bb61c831472de21e3550a3e69ff1
Beau Guthery <bguthery@gmail.com>
2024-05-20 12:17:01 -0500

    Comment changes

--- Coding/Python/Reading_List/Reading_List.py
+++ Coding/Python/Reading_List/Reading_List.py
@@ -6,16 +6,16 @@ import os
 import random
 
 
-# Setting up the absolute path for the txt list
+# Setting up the absolute path for the file
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 file_path = os.path.join(os.getcwd(), "Reading List.txt")
 
-# Function to add spacing 
+# Spacing function
 def space():
     print(" ")
 
-# Function for situations where I don't want to immediatly bring the user back to the main menu
-def continuation_prompt():
+# Exit prompt function
+def exit_prompt():
     
     while True:
         answer = input("Would you like to go back to the main menu? (Y/N): ").strip().lower()
@@ -28,7 +28,7 @@ def continuation_prompt():
                 
         else:
             print("Please enter a valid option.")
-            continuation_prompt()
+            exit_prompt()
 
 
 while True:
@@ -45,7 +45,7 @@ while True:
         space()
 
 
-        # TODO: Add the ability to enter a "mass title" mode which will let you enter titles after title without any prompts
+        # TODO: Add the ability to enter a "mass title" mode which will let you enter title after title without any prompts or fewer prompts
         if answer in ["a", "add"]:
         
             with open("Reading List.txt",'a') as file:
@@ -76,18 +76,17 @@ while True:
 
             with open("Reading List.txt", 'r') as file:
 
-                # Read all lines from the file into the 'lines' variable and get the size of the file into the 'file_size' variable 
+                # Reads all lines from the file into the 'lines' variable and gets the size of the file into the 'file_size' variable 
                 lines = file.readlines()
                 file_size = os.path.getsize(file_path)
 
                 # TODO: The ability to select a specific amount of random titles
-                
                 # Checks to see if the file has any content inside or not
                 if (file_size == 0):
                     space()
                     print("The list is empty")
                     space()
-                    continuation_prompt()
+                    exit_prompt()
                     space()
                 elif (file_size > 0):
                     choice = random.choice(lines).strip()
@@ -113,7 +112,7 @@ while True:
                     space()
                     print("List has been deleted!")
                     space()
-                    continuation_prompt()   
+                    exit_prompt()   
                 elif answer in ["E", "Exit"]:
                     space()
                     break
