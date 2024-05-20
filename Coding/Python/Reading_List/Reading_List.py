c4e55260f204b6f185f3f22a747fa414cba29c5f
Beau Guthery <bguthery@gmail.com>
2024-05-19 22:15:16 -0500

    New functions
    enabled users to delete the list from the menus and added the ability to pull a random title out of the list.

--- Coding/Python/Reading_List/Reading_List.py
+++ Coding/Python/Reading_List/Reading_List.py
@@ -3,38 +3,63 @@
 #Imports
 import sys
 import os
+import random
 
-#Absolute path to the Reading List.txt
+
+#Setting up the absolute path for the txt list
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 file_path = os.path.join(os.getcwd(), "Reading List.txt")
 
-#Function that will ask users if they wish to enter more titles or exit the program
 def continuation_prompt():
     
     while True:
-        answer = input("Would you like to add another title? (Y/N): ").strip().lower()
+        answer = input("Would you like to go back to the main menu? (Y/N): ").strip().lower()
             
         if answer in ["y", "yes"]:
             break
         
-        elif answer in ["n", "n"]:
+        elif answer in ["n", "no"]:
             sys.exit()
                 
         else:
             print("Please enter a valid option.")
             continuation_prompt()
-            
 
 while True:
+        
+        answer = input("Please select a option (A)dd/(R)andom? or type 'delete now' to delete list: ").strip().lower()
+    
+        if answer in ["a", "add"]:
+        
+            with open("Reading List.txt",'a') as file:
 
-    with open("Reading List.txt",'a') as file:
-        
-        answer = input("Please add the title of the story you wish to add to your 'Reading List' list:\n")
+                answer = input("Please add the title of the story you wish to add to the list:\n").strip().title()
+            
+                file.write(f"[{answer}]" + '\n')
 
-        #Takes the answer variable which stores the title you wish to add to the list and writes it on a new line in the txt file
-        file.write(f"[{answer}]" + '\n')
+                print("<Title is now saved>")
+
+            continuation_prompt()
         
-        print("<Title is now saved>")
-        
-        file.close()
-        continuation_prompt()
+        elif answer in ["r", "random"]:
+
+            with open("Reading List.txt", 'r') as file:
+
+                #using Random library to pull a random title out of the txt list
+                lines = file.readlines()
+                choice = random.choice(lines).strip()
+
+                print(choice)
+
+            continuation_prompt
+
+        elif answer == "delete now":
+                    
+            open("Reading List.txt", "w").close()
+
+            print("List has been deleted!")
+            continuation_prompt()   
+
+        else:
+            print("Please select a valid entry.")
+            continuation_prompt
