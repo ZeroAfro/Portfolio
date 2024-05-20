8838f0db37f39fc9736d4f753be4903c88a8551c
Beau Guthery <bguthery@gmail.com>
2024-05-20 13:44:47 -0500

    Combined both earnings and overtime calcs
    Combined the functions of both these programs into one program

--- Coding/Python/Overtime_Calculator/README.md
+++
@@ -1,3 +0,0 @@
-Small overtime calculator as mini project to learn about conditionals and booleans
-takes the hours worked and checks to see if its more than or less than 40, if its more than 40 it subtracts 40 from the hours worked and multiplies at by 1.5 to find the amount of overtime pay you are owed
-if the hours worked are less than then it tells you that you are not owed any overtime pay and asks if you would like to exit
---
+++ Coding/Python/Wage_Calculator/Wage_Calculator.py
@@ -0,0 +1,93 @@
+# Wage Calculator
+
+# TODO: Add exit confirmations and statements
+# Library Imports
+from sys import exit
+
+#Function to add space
+def space():
+    print(" ")
+
+
+print("Welcome to the wage calcuator!")
+space()
+
+# TODO: Add exit option to main menu
+while True:
+    
+    answer = input("Would you like to calculate (R)egular or (O)vertime wages?: ").strip().lower()
+    space()
+    
+    while True: 
+        
+        if answer in ["r", "regular"]:
+    
+            try:            
+                hours_worked = float(input("How many hours did you work?: "))
+                space()
+                
+                while True:
+                    try:
+                        hourly_wage = float(input("What's your hourly wage?: "))
+                        break
+                    except ValueError:
+                        space()
+                        print("Please enter a valid number.")
+                        space()
+                        
+                earnings = hours_worked * hourly_wage
+                
+                space()
+                print(f"You earned: ${earnings:.2f}")
+                space()
+                break
+            
+            except ValueError:
+                space()
+                print("Please enter a valid number.")
+                space()
+            
+    
+        elif answer in ["o", "overtime"]:
+        
+            try:
+                hours_worked = float(input("How many hours did you work?: "))
+                space()
+            
+                if hours_worked > 40:
+                    
+                    while True:
+            
+                        try:           
+            
+                            hourly_wage = float(input("What's your hourly wage?: "))
+                            space()
+                            break
+                        
+                        except ValueError:
+                            space()
+                            print("Please enter a valid number.")
+                            space()
+            
+                    overtime_hours = float(hours_worked - 40)
+                    overtime_pay = float(overtime_hours * 1.5 * hourly_wage)
+                    
+                    print(f"You earned: ${overtime_pay:.2f} in overtime.")
+                    space()
+                    break
+                      
+                
+                elif hours_worked < 40:
+                    print("You worked less than 40 hours.")
+                    space()
+                    break
+                else:
+                    print("Please entera valid number.")
+                    space()
+            except ValueError:
+                space()
+                print("Please enter a valid number.")
+                space()
+        else:
+            print("Please enter a valid option")
+            space()
