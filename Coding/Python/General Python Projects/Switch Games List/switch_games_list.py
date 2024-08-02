e0c41abf1a47b8c6e5b857aee407ba0790386776
Beau Guthery <bguthery@gmail.com>
2024-08-02 16:34:34 -0500

    Update to switch_games_list.py
    Added major error handling

--- Coding/Python/General Python Projects/Switch Games List/switch_games_list.py
+++ Coding/Python/General Python Projects/Switch Games List/switch_games_list.py
@@ -1,23 +1,72 @@
-"""Tracks and sorts switch games"""
+"""Tracks and Sorts Switch Games to a list in JSON format"""
 
 from pathlib import Path
 import json
 
 
 def main():
+    """Main function, sets path and checks for errors and if path exists"""
+
     path = Path("switch_game_list.json")
     if path.exists():
-        try:
-            contents = path.read_text()
-            titles = json.loads(contents)
-        except (json.JSONDecodeError, IOError):
+        if path.stat().st_size > 0:
+            try:
+                contents = path.read_text()
+                titles = json.loads(contents)
+            except (json.JSONDecodeError, IOError):
+
+                titles = main_error_handling(path)
+
+        else:
+            add_line_break()
+            print(f"The file [{path}] is currently empty. Initializing with "
+                  "an empty list.")
+            add_line_break()
             titles = []
+            path.write_text(json.dumps(titles))
+
     else:
         titles = []
+        path.write_text(json.dumps(titles))
+
     return titles, path
 
 
+def main_error_handling(path):
+    """Handles the majority of the error checking for main function"""
+
+    add_line_break()
+    print(f"Detected a problem with trying to read the file: [{path}].")
+    add_line_break()
+
+    prompt = input("Would you like to try to override and rewrite the "
+                   "file? (y/n): ").strip()
+
+    if prompt == "y".lower():
+        titles = []
+        path.write_text(json.dumps(titles))
+        add_line_break()
+        print(f"File [{path}] has been rewritten.")
+        add_line_break()
+
+    elif prompt == "n".lower():
+        add_line_break()
+        print(f"Please check the file {path} for any errors such as invalid "
+              "JSON strings.")
+        add_line_break()
+        quit()
+
+    else:
+        add_line_break()
+        print("Please enter a valid option of either 'y' or 'n'.")
+        add_line_break()
+
+    return titles
+
+
 def add_line_break():
+    """Adds spacing for better formatting"""
+
     print("\n")
 
 
@@ -37,18 +86,23 @@ if __name__ == "__main__":
 
         if switch_title.lower() == "q":
             titles.sort()
-            path.write_text(json.dumps(titles, ensure_ascii=False, indent=4))
-            break
+            try:
+                path.write_text(json.dumps(titles, ensure_ascii=False,
+                                           indent=4))
+                break
+            except PermissionError:
+                add_line_break()
+                print("You do not have permission to save to the file: "
+                      f"[{path}]"
+                      "\nPlease check your user/folder permissions.")
+                add_line_break()
+                break
 
         elif switch_title.lower() == "v":
-            try:
-                add_line_break()
-                for title in titles:
-                    print(f"\t{title}")
-                add_line_break()
-            except (json.JSONDecodeError, IOError):
-                print("Error reading the list.")
-                add_line_break()
+            add_line_break()
+            for title in titles:
+                print(f"\t{title}")
+            add_line_break()
 
         elif switch_title.isdigit():
             add_line_break()
@@ -56,12 +110,19 @@ if __name__ == "__main__":
             add_line_break()
 
         else:
-            saved_titles = json.loads(path.read_text())
+            try:
+                saved_titles = json.loads(path.read_text())
+            except PermissionError:
+                add_line_break()
+                print(f"You do not have permission to load the file [{path}]\n"
+                      "Please check your user/folder permissions.")
+                add_line_break()
+                break
 
             if switch_title in titles or switch_title in saved_titles:
                 add_line_break()
                 print("That title is already in your list, "
-                      "please check and try again.")
+                      "please check it and try again.")
                 add_line_break()
 
             else:
